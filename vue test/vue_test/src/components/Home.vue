e8c65368f8016f9203dd1ec7f09f3e5faea8c617
Yuki <daisy99210@126.com>
2020-07-19 19:32:59 +0800

    delete branch:branch_test

--- vue test/vue_test/src/components/Home.vue
+++ vue test/vue_test/src/components/Home.vue
@@ -1,9 +1,21 @@
 <template>
-    <div>
-        <el-button type="info" @click="logout">
-            退出
-        </el-button>
-    </div>
+    <el-container class="home-container">
+        <!-- 头部区域 -->
+        <el-header>
+            <div>
+                <img src="" alt="">
+                <span>疫情可视化</span>
+            </div>
+            <el-button type="info" @click="logout">退出</el-button>
+        </el-header>
+        <!-- 页面主体区 -->
+        <el-container>
+            <!-- 侧边栏 -->
+            <el-aside width="200px">Aside</el-aside>
+            <!-- 右侧内容主体 -->
+            <el-main>Main</el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
@@ -18,5 +30,16 @@
 </script>
 
 <style lang="less" scoped>
-
+    .home-container{
+        height:100%;
+    }
+    .el-header {
+        background-color: #373D41;
+    }
+    .el-aside{
+        background-color:#333744;
+    }
+    .el-main{
+        background-color:#EAEDF1;
+    }
 </style>
--- vue test/vue_test/src/plugins/element.js
+++ vue test/vue_test/src/plugins/element.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import { Button } from 'element-ui'
 import { Form, FormItem } from 'element-ui'
-import { Input } from 'element-ui'
+import { Input, Container, Header, Aside, Main } from 'element-ui'
 // 导入弹框提示组件
 import { Message } from 'element-ui'
 
@@ -9,4 +9,8 @@ Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
 Vue.prototype.$message = Message
